dcd12584b22b6b0e41686613638e85fbc6664786
scossar <scossar3@gmail.com>
2016-06-18 23:34:51 -0700

    use valid locale format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -334,7 +334,7 @@ class ApplicationController < ActionController::Base
         # Rails I18n uses underscores between the locale and the region; the request
         # headers use hyphens.
         require 'http_accept_language' unless defined? HttpAcceptLanguage
-        available_locales = ['en', 'fr', 'fa-IR']
+        available_locales = I18n.available_locales.map { |locale| locale.to_s.tr('_', '-') }
         parser = HttpAcceptLanguage::Parser.new(request.env["HTTP_ACCEPT_LANGUAGE"])
         parser.language_region_compatible_from(available_locales).tr('-', '_')
       rescue
